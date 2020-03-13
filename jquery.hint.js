133d0c970f23344e292fed2022a13fdfa1363e23
Andrii <48489409+andrii-gh@users.noreply.github.com>
2020-03-13 07:39:13 +0200

    init

---
+++ jquery.hint.js
@@ -0,0 +1,59 @@
+(function ($) {
+
+    $.fn.ybkHint = function (action, options) {
+        switch (action) {
+            case 'show': {
+                options = $.extend({
+                    hideTimeout: 2000
+                }, options);
+
+                show(options);
+
+                break;
+            }
+
+            case 'hide': {
+                hide(options);
+
+                break;
+            }
+        }
+    };
+
+    function show(options) {
+        const name = options.name;
+
+        const template = `
+            <div
+                data-ybk-hint="${name}"
+                data-ybk-hint-state="expanded"
+                class="ybk-popup-hint-element-loading ybk-popup-hint-element-loading_theme-default">
+                    <div class="ybk-popup-hint-element-loading__content ybk-popup-hint-element-loading__content_theme-default">
+                       
+                       ${options.addPreloader
+                            ? `<div class="ybk-popup-hint-element-loading-content__preloader ybk-popup-hint-element-loading-content__preloader_theme-default"></div>`
+                            : '' }
+                       
+                        <div class="ybk-popup-hint-element-loading-content__text ybk-popup-hint-element-loading-content__text_theme-default">
+                            ${options.content}
+                        </div>
+                    </div>
+            </div>
+        `;
+
+        $('body').append(template);
+
+        setTimeout(() => {
+            hide(options);
+        }, options.hideTimeout)
+    }
+
+    function hide(options) {
+        $(`[data-ybk-hint="${options.name}"]`)
+            .attr('data-ybk-hint-state', 'collapsed')
+            .on('animationend', function() {
+                $(this).remove();
+            });
+    }
+
+}(jQuery));
